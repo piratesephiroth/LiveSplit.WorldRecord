9791b0a60ad25bdd2cc1338b92cb9358d4eb7fc4
Christopher Serr <cryze92@gmail.com>
2015-02-17 01:29:57 +0100

    Added the Component and Factory Classes

---
+++ UI/Components/WorldRecordComponent.cs
@@ -0,0 +1,101 @@
+﻿using LiveSplit.Model;
+using LiveSplit.UI;
+using LiveSplit.UI.Components;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace LiveSplit.WorldRecord.UI.Components
+{
+    public class WorldRecordComponent : IComponent
+    {
+        public string ComponentName
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public float HorizontalWidth
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public float MinimumHeight
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public float VerticalHeight
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public float MinimumWidth
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public float PaddingTop
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public float PaddingBottom
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public float PaddingLeft
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public float PaddingRight
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IDictionary<string, Action> ContextMenuControls
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void DrawHorizontal(Graphics g, LiveSplitState state, float height, Region clipRegion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawVertical(Graphics g, LiveSplitState state, float width, Region clipRegion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Control GetSettingsControl(LayoutMode mode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public XmlNode GetSettings(XmlDocument document)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetSettings(XmlNode settings)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UI/Components/WorldRecordFactory.cs
@@ -0,0 +1,54 @@
+﻿using LiveSplit.Model;
+using LiveSplit.UI.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+[assembly:ComponentFactory(typeof(LiveSplit.WorldRecord.UI.Components.WorldRecordFactory))]
+
+namespace LiveSplit.WorldRecord.UI.Components
+{
+    public class WorldRecordFactory : IComponentFactory
+    {
+        public string ComponentName
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string Description
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public ComponentCategory Category
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IComponent Create(LiveSplitState state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string UpdateName
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string XMLURL
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string UpdateURL
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public Version Version
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
