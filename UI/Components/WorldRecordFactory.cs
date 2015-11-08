9da3602466dff58ac03118b8de367ac64b782b95
Sergey Papushin <spapushin@gmail.com>
2015-11-08 16:19:42 -0600

    Update version to 1.6.3

--- UI/Components/WorldRecordFactory.cs
+++ UI/Components/WorldRecordFactory.cs
@@ -22,6 +22,6 @@ namespace LiveSplit.WorldRecord.UI.Components
 
         public string UpdateURL => "http://livesplit.org/update/";
 
-        public Version Version => Version.Parse("1.6.1");
+        public Version Version => Version.Parse("1.6.3");
     }
 }
