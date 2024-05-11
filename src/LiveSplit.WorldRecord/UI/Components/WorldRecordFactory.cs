13c75fd911758adcb9c945653e8d848da7c7d931
wooferzfg <spapushin@gmail.com>
2024-05-11 09:48:40 -0400

    Update version to 1.8.29

--- src/LiveSplit.WorldRecord/UI/Components/WorldRecordFactory.cs
+++ src/LiveSplit.WorldRecord/UI/Components/WorldRecordFactory.cs
@@ -22,6 +22,6 @@ namespace LiveSplit.WorldRecord.UI.Components
 
         public string UpdateURL => "http://livesplit.org/update/";
 
-        public Version Version => Version.Parse("1.8.21");
+        public Version Version => Version.Parse("1.8.29");
     }
 }
